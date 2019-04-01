035f36883b81ffd0199ab1c2313e366b450f6983
hoang <vinhyenvodoi98@gmail.com>
2019-04-01 16:29:32 +0700

    add winorlose and viewResult function

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -7,31 +7,42 @@ contract CoinFlip {
     uint256 lastHash;
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
     address owner = 0x4C637fC36ecA2d02d5214b53c0aEc272f31F7E53;
+    uint8 winorlose = 0;
 
     constructor () public {
         consecutiveWins = 0;
     }
 
     // try to use other random algorithm
-    function flip(bool _guess ) public payable returns (bool) {
+    function flip(bool _guess ) public payable {
+        winorlose = 0; // set new game
         // require(msg.value > ); //set require value
         uint256 blockValue = uint256(blockhash(block.number-1));
 
         if (lastHash == blockValue) {
-        revert();
+            revert();
         }
 
         lastHash = blockValue;
         uint256 coinFlip = blockValue / FACTOR;
         bool side = coinFlip == 1 ? true : false;
 
-        if (side == _guess) {
-        consecutiveWins++;
-        return true;
-        } else {
-        consecutiveWins = 0;
-        return false;
+        if(side){
+            winorlose = 1 ;  // 1 mean sap
+        }else{
+            winorlose = 2 ;  // 2 mean ngua
         }
+
+        if (side == _guess) {
+            consecutiveWins++;
+        } else {
+            consecutiveWins = 0;
+        }
+    }
+
+    // view result 
+    function viewResult() public view returns(uint8) {
+        return winorlose;
     }
 
     // view how many time you win
