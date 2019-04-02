2d3785c3019ba04042d731ed41d0899cb4ba2134
hoang <vinhyenvodoi98@gmail.com>
2019-04-02 17:11:24 +0700

    update contract have event(matchNumber,win)

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -3,12 +3,14 @@
 pragma solidity ^0.4.24;
 
 contract CoinFlip {
-    event closeflip(uint8 winorlose);
+    //add matchNumber to finter match
+    event closeflip(uint256 matchNumber, uint8 winorlose);
     uint256 public consecutiveWins;
     uint256 lastHash;
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
     address owner = 0x4C637fC36ecA2d02d5214b53c0aEc272f31F7E53;
     uint8 winorlose = 0;
+    uint256 matchNumber = 0;
 
     constructor () public {
         consecutiveWins = 0;
@@ -16,7 +18,7 @@ contract CoinFlip {
 
     // try to use other random algorithm
     function flip(bool _guess ) public payable {
-        
+        matchNumber ++ ;
         winorlose = 0; // set new game
         // require(msg.value > ); //set require value
         uint256 blockValue = uint256(blockhash(block.number-1));
@@ -37,10 +39,10 @@ contract CoinFlip {
 
         if(side){ 
             winorlose = 1;
-            emit closeflip (winorlose);
+            emit closeflip (matchNumber,winorlose);
         }else{
             winorlose = 2;
-            emit closeflip (winorlose);
+            emit closeflip (matchNumber,winorlose);
         }
     }
 
