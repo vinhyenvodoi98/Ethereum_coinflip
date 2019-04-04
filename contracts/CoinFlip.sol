5b9c5949811deaf94e98f2e86c21335232481a7d
hoang <vinhyenvodoi98@gmail.com>
2019-04-04 08:50:15 +0700

    update contract and build

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -6,25 +6,24 @@ pragma solidity ^0.4.24;
 contract CoinFlip {
     //add matchNumber to finter match
 
-    event closeflip(uint256 matchNumber, uint8 winorlose);
+    event closeflip(uint256 matchNumber, uint winorlose);
 
     mapping(address => uint) public winNumber;
+    mapping(address => uint) public winloseNumber;
 
-    uint256 public consecutiveWins;
     uint256 lastHash;
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
     address owner = 0x4C637fC36ecA2d02d5214b53c0aEc272f31F7E53;
-    uint8 winorlose = 0;
     uint256 matchNumber = 0;
  
     constructor () public {
         winNumber[msg.sender] = 0;
+        winloseNumber[msg.sender] = 0;
     }
 
     // try to use other random algorithm
     function flip(bool _guess ) public payable {
         matchNumber ++ ;
-        winorlose = 0; // set new game
         // require(msg.value > ); //set require value
         uint256 blockValue = uint256(blockhash(block.number-1));
 
@@ -43,11 +42,11 @@ contract CoinFlip {
         }
 
         if(side){ 
-            winorlose = 1;
-            emit closeflip (matchNumber,winorlose);
+            winloseNumber[msg.sender] = 1;
+            emit closeflip (matchNumber,winloseNumber[msg.sender]);
         }else{
-            winorlose = 2;
-            emit closeflip (matchNumber,winorlose);
+            winloseNumber[msg.sender] = 2;
+            emit closeflip (matchNumber,winloseNumber[msg.sender]);
         }
     }
 
@@ -59,8 +58,8 @@ contract CoinFlip {
     }
 
     // view result 
-    function viewResult() public view returns(uint8) {
-        return winorlose;
+    function viewResult() public view returns(uint) {
+        return winloseNumber[msg.sender];
     }
 
     // view how many time you win
